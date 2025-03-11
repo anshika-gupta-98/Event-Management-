3983ecf1a1c553f195dbb27742990465027fce25
Anshika Gupta <132778691+anshika-gupta-98@users.noreply.github.com>
2025-03-11 17:16:51 +0530

    Added app.js

---
+++ app.js
@@ -0,0 +1,114 @@
+
+
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
+let themeToggler = document.querySelector(".theme-toggler");
+let toggleBtn = document.querySelector(".toggle-btn");
+
+toggleBtn.onclick = () => {
+  themeToggler.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  themeToggler.classList.remove("active");
+};
+
+document.querySelectorAll(".theme-toggler .theme-btn").forEach((btn) => {
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector(":root").style.setProperty("--theme-color", color);
+  };
+});
+
+document.querySelectorAll(".bg-btn").forEach((btn) => {
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector("body").style.backgroundColor = color;
+  };
+});
+
+var swiper = new Swiper(".home-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".review-slider", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1050: {
+      slidesPerView: 3,
+    },
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
